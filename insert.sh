7e28067004b577fc3aa57f2ce5050068ce72aaf1
Juan carlos <juan.aristizabal@and.gov.co>
2022-12-01 16:43:44 -0500

    update

--- insert.sh
+++ insert.sh
@@ -3,8 +3,8 @@
 PATH_NAME_1=~/$XROAD_FOLDER/src/center-ui/app/controllers/configuration_management_controller.rb
 PATH_NAME_2=~/$XROAD_FOLDER/src/center-service/app/models/configurations_generator.rb
 
-cp ./configuration_management_controller.rb  PATH_NAME_1
-cp ./configurations_generator.rb  PATH_NAME_2
+cp ./configuration_management_controller.rb  $PATH_NAME_1
+cp ./configurations_generator.rb  $PATH_NAME_2
 
 if [ -f "$PATH_NAME_2" ]; then
     echo "everything's ok..."
